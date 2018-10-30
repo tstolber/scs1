c84d68dd1b3cf3cdc877d58aa4deeb595b897e33
Trevor Stolber <tstolber@gmail.com>
2018-10-29 22:18:32 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Main.py
+# This is the main entry point into the tool set
+# Created by: Trevor Stolber on 2018-10-29
+# V0.0.1 - Initial Version - 2018-10-29
+
+"""
+Input Options for CLI - option drive menu
+
+
+
+"""
+
+
+
