8ebd94d62eb9775a84f45b01d698a934e4b66cc7
Trevor Stolber <tstolber@gmail.com>
2018-10-29 22:33:01 -0500

    Create penLib.py
    This is the main entry functions library fpr the tool set
    V0.0.1 - Initial Version - 2018-10-29

---
+++ penLib.py
@@ -0,0 +1,21 @@
+# penLib.py
+# This is the main entry functions library fpr the tool set
+# Created by: Trevor Stolber on 2018-10-29
+# V0.0.1 - Initial Version - 2018-10-29
+
+"""
+Library Functions
+"""
+
+# Default basic port scanning function
+def portScan(host,range):
+    print (host)
+    return range
+
+# Response Code Function
+def getResponseCode(url):
+    print (url)
+    return code
+
+
+
