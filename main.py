d99530d53ebfc2eed68c82ace5824fac2db13e56
Trevor Stolber <tstolber@gmail.com>
2018-10-29 22:26:25 -0500

    Update main.py
    # V0.0.2 - Added options and variables 2018-10-29

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 # Main.py
 # This is the main entry point into the tool set
 # Created by: Trevor Stolber on 2018-10-29
+# V0.0.2 - Added options and variables 2018-10-29
 # V0.0.1 - Initial Version - 2018-10-29
 
 """
 Input Options for CLI - option drive menu
 
+Option 1) Recon
 
+TODO: Add command line handler to take in arguments
+TODO: Input and out put file handlers
+TODO: CL Ooptions handler
 
 """
 
+print ("SCS 1")
+print ("Pen Test Suite")
 
+host = "127.0.0.1" # List target host
+urlsFile = "" # Location of a URLs file
+outPutFile = "" # Results output file
 
